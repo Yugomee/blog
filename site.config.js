9bccb13519d1b23dc28012cb774bc546148f95b3
Yugomee <jeehyunee625@gmail.com>
2023-03-05 21:32:36 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "bong-yee",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
+    role: "booong",
     bio: "I develop everything using node.",
     email: "morethanmin.dev@gmail.com",
     linkedin: "morethanmin",
